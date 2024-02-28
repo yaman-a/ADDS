ed202754df68c9d480e40cc67738a5c5f580f0e5
root <root@Inspiron>
2024-02-28 10:37:56 +1030

    Started practical

---
+++ player.cpp
@@ -0,0 +1,16 @@
+#include "player.h"
+#include <bits/stdc++.h>
+
+player::player() {
+}
+
+char player::makeMove() {
+
+}
+
+string player::getName() {
+
+}
+
+player::~player() {
+}
---
+++ player.h
@@ -0,0 +1,20 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class player {
+private:
+    /* data */
+public:
+    player();
+    ~player();
+    char makeMove();
+    string getName();
+};
+
+
+
+#endif // PLAYER_H
