827f33e02405e984e4334cac82bafd7fbe71f290
Yaman Ashqar <a1884774@ingg22w022.ad.adelaide.edu.au>
2024-05-01 12:29:58 +0930

    bruh

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -35,22 +35,90 @@ void LinkedList::printList() {
 /////////// your code goes here... DO NOT change the function signatures ///////////
 
 bool LinkedList::swap(int pos1, int pos2) {
-	Node* node1 = head;
-	Node* node2 = head;
+    
+    Node* temp1 = head;
+    Node* node1 = temp1;
 
-	Node* track1 = head;
-	Node* prev1 = track1;
+    int n1 = 0;
 
-	int i = 0;
+    if (pos1 == 0) {
+        node1 = head;
+        temp1 = node1;
+    } else {
+        while (temp1 != nullptr) {
+            if (n1 == pos1) {
+                node1 = temp1->link;
+                break;
+            }
 
-	while (i < pos1) {
-		track1 = track1->link;
-	}
+            temp1 = temp1->link;
+            n1++;
+        }
+    }
 
-	node1 = track1;
+    Node* temp2 = head;
+    Node* node2 = temp2;
+
+    int n2 = 0;
+
+    if (pos2 == 0) {
+        node2 = head;
+        temp2 = node2;
+    } else {
+        while (temp1 != nullptr) {
+            if (n2 == pos2) {
+                node1 = temp2->link;
+                break;
+            }
+
+            temp2 = temp2->link;
+            n2++;
+        }
+    }
+
+    Node* new1 = node2;
+    Node* new2 = node1;
+
+    temp1->link = new1;
+    new1->link = node1->link;
+
+    temp2->link = new2;
+    new2->link = node2->link;
+
+    return true;
+    
+
 
 }
 
 bool LinkedList::find_and_delete(int target) {
+    if (head == nullptr) {
+        return false;
+    }
 
+    Node* temp = head;
+
+    if (head->data == target) {
+        head = temp->link;
+        delete temp;
+        return true;
+    }
+
+    Node* deleted = temp;
+
+    while (temp != nullptr) {
+        if (temp->link == nullptr) {
+            return false;
+        }
+        if (temp->link->data == target) {
+            deleted = temp->link;
+            break;
+        }
+        temp = temp->link;
+    }
+
+    Node* next = deleted->link;
+    temp->link = next;
+    delete deleted;
+    return true;
 }
---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+#include "LinkedList.h"
+#include "Node.h"
+
+
+int main() {
+
+    LinkedList L;
+
+    L.insertFront(1);
+    L.insertFront(2);
+    L.insertFront(3);
+    L.insertFront(4);
+    L.insertFront(5);
+
+    L.printList();
+
+    std::cout << std::endl;
+
+    bool status = L.swap(1, 3);
+
+    std::cout << status << std::endl;
+    L.printList();
+
+
+
+
+}
