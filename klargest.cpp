86992ae7d0af292c345555e8cf11bc49ac943a9a
Arab-Man <yaman.ashqar05@gmail.com>
2024-05-19 18:28:15 +0930

    attempt 2

--- klargest.cpp
+++ klargest.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <cmath>
+#include <algorithm>
 
 int kth_largest(std::vector<int> values, int k) {
     std::priority_queue<int, std::vector<int>, std::greater<int>> min_heap;
@@ -12,6 +14,6 @@ int kth_largest(std::vector<int> values, int k) {
     for (int i = 0; i < (min_heap.size() - k); i++) {
         min_heap.pop();
     }
-    
+
     return min_heap.top();
 }
