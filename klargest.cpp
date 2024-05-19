f56f4571a5f2c8fc867790aeb3b1c83e71dbf3a6
Arab-Man <yaman.ashqar05@gmail.com>
2024-05-19 18:39:14 +0930

    added main to klargest

--- klargest.cpp
+++ klargest.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <cmath>
 #include <algorithm>
+#include <functional>
 
 int kth_largest(std::vector<int> values, int k) {
     std::priority_queue<int, std::vector<int>, std::greater<int>> min_heap;
 
     for (int i : values) {
-        if (min_heap.size() < k) {
+        int size = min_heap.size();
+        int top = min_heap.top();
+        if (size < k) {
             min_heap.push(i);
-        } else if (i > min_heap.top()) {
+        } else if (i > top) {
             min_heap.pop();
             min_heap.push(i);
         }
     }
 
     return min_heap.top();
+}
+
+int main() {
+    
 }
